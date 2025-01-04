8a6f5a456ca13b62bdd9e5cd27c4145d6c749679
FelipeParraC <felipeparra20@gmail.com>
2025-01-04 17:39:06 -0500

    Creación de useTransactionStore

---
+++ src/store/transaction/transaction-store.ts
@@ -0,0 +1,33 @@
+import { Transaction } from '@/interfaces'
+import { create } from 'zustand'
+
+
+
+interface TransactionState {
+
+    transactions: Transaction[]
+
+    setTransactions: (transactions: Transaction[]) => void
+    addTransaction: (transaction: Transaction) => void
+    updateTransaction: (transaction: Transaction) => void
+    removeTransaction: (id: string) => void
+}
+
+export const useTransactionStore = create<TransactionState>((set) => ({
+
+    transactions: [],
+    
+    setTransactions: (transactions) => set({ transactions }),
+    addTransaction: (transaction) =>
+        set((state) => ({ transactions: [...state.transactions, transaction] })),
+    updateTransaction: (transaction) =>
+        set((state) => ({
+            transactions: state.transactions.map((t) =>
+                t.id === transaction.id ? transaction : t
+            ),
+        })),
+    removeTransaction: (id) =>
+        set((state) => ({
+            transactions: state.transactions.filter((t) => t.id !== id),
+        })),
+}))
